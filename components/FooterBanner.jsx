168922e17335a038c1fe2c6cf129b7c666a145bf
unknown <git@github.com:VicThorMetaNode>
2022-10-17 20:29:04 +0200

    footer banner api connection

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import Link from "next/link";
 
-const FooterBanner = () => {
-  return <div>FooterBanner</div>;
+//Import IMAGES from client
+import { urlFor } from "../lib/client";
+
+// IMPORT CHAKRA tools
+import { Box, Flex, Stack, Text, Button } from "@chakra-ui/react";
+
+const FooterBanner = ({
+  footerBanner: {
+    discount,
+    largeText1,
+    largeText2,
+    saleTime,
+    smallText,
+    midText,
+    desc,
+    product,
+    buttonText,
+    image,
+  },
+}) => {
+  return (
+    <>
+      <section className="footer-banner-section">
+        <Box className="footer-banner-desc-box">
+          <Box className="footer-banner-desc-left">
+            <Text className="footer-banner-desc-discount">{discount}</Text>
+            <Text className="footer-banner-desc-largeText1">{largeText1}</Text>
+            <Text className="footer-banner-desc-largeText2">{largeText2}</Text>
+            <Text className="footer-banner-desc-saleTime">{saleTime}</Text>
+          </Box>
+          <Box className="footer-banner-desc-right">
+            <Text className="footer-banner-desc-smallText">{smallText}</Text>
+            <Text className="footer-banner-desc-midText">{midText}</Text>
+            <Text className="footer-banner-desc-desc">{desc}</Text>
+            <Link href={`/product/${product}`}>
+              <Button>{buttonText} </Button>
+            </Link>
+          </Box>
+          <Box>
+            <img
+              src={urlFor(image)}
+              height="400"
+              width="400"
+              className="footer-banner-image"
+            />
+          </Box>
+        </Box>
+      </section>
+    </>
+  );
 };
 
 export default FooterBanner;
